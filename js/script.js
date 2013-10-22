a913afd41671f33e281852dc8fb6730f43aa4dc7
zanget81 <nagel81@gmail.com>
2013-10-22 17:47:06 +0200

    Adding initial version of the app developed on hackfest just some js function, still more a js tutorial than anything else

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function wikipediaSearch(searchTerm) 
+{
+	console.log( "trying to do a request" );
+	$.ajax({
+		url:"http://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=" + searchTerm +	"&format=json&gsrprop=snippet&prop=info&inprop=url",
+		crossDomain: true,
+		success:function(json)
+		{
+			console.log("search: " + searchTerm + " success");
+			console.log("total hits: " + json['query']['searchinfo']['totalhits']);
+			return json['query']['searchinfo']['totalhits'];
+		},
+		error:function()
+		{
+			console.log("search: " + searchTerm + "error");
+			return 0;
+		},
+	});
+}
